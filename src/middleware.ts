e1fb9ad37755f13594734352c74770f45d7fb4a7
Karan0805 <karangandhi.navsari@gmail.com>
2024-04-30 03:51:33 +0530

    feat: Ecommerce Platform using Nextjs, Typescript and Tailwind CSS

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextMiddleware, NextResponse } from 'next/server';
-import withAuthorization from './middlewares/withAuthorization';
-
-const mainMiddleware: NextMiddleware = () => {
-  const res = NextResponse.next();
-  //other middleware operations
-  return res;
-};
-export default withAuthorization(mainMiddleware, ['/admin']);
